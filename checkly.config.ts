85caa85d583d91b4eb1a23abd2849811970e1271
María de Antón <maria@checklyhq.com>
2025-05-27 17:27:53 +0200

    Update checkly.config.ts

--- checkly.config.ts
+++ checkly.config.ts
@@ -7,17 +7,17 @@ export default defineConfig({
  logicalId: 'pwExamples-Maria',
  repoUrl: 'https://github.com/MariadeAnton/playwright-examples',
  checks: {
- playwrightConfigPath: './playwright.config.ts',
- include: ['.npmrc'],
- playwrightChecks: [
-	  {
+     playwrightConfigPath: './playwright.config.ts',
+     include: ['.npmrc'],
+     playwrightChecks: [
+     {
 	    //Run the Chromium project with checkly configuration
 	    name: 'Chromium Suite',
 	    logicalId: 'chromium-e2e-suite',
 	    pwProjects: 'chromium', // Reference a project in your playwright.config.ts
 	    frequency: Frequency.EVERY_10M,
-		locations:['us-west-1','eu-west-2','ap-northeast-1'],
-          },
+	    locations:['us-west-1','eu-west-2','ap-northeast-1'],
+      },
 	  {
 	    name: 'Critical suite in all device projects',
 	    logicalId:'critical-suite',
@@ -32,7 +32,7 @@ export default defineConfig({
 	    pwTags: 'sanity',  // Reference a tag in your tests
 	    frequency:  Frequency.EVERY_2M,
 	  },
-	 ],
+     ],
  },
  //also include:
  cli: {
