362011726b25150181668aab6b4518e6df943766
María de Antón <maria@checklyhq.com>
2025-05-27 17:25:22 +0200

    correct indentation

--- checkly.config.ts
+++ checkly.config.ts
@@ -13,22 +13,22 @@ export default defineConfig({
 	  {
 	    //Run the Chromium project with checkly configuration
 	    name: 'Chromium Suite',
-		logicalId: 'chromium-e2e-suite',
+	    logicalId: 'chromium-e2e-suite',
 	    pwProjects: 'chromium', // Reference a project in your playwright.config.ts
 	    frequency: Frequency.EVERY_10M,
 		locations:['us-west-1','eu-west-2','ap-northeast-1'],
-      },
+          },
 	  {
 	    name: 'Critical suite in all device projects',
-		logicalId:'critical-suite',
+	    logicalId:'critical-suite',
 	    pwTags: 'critical',  // Reference a tag in your tests
 	    frequency:  5,
-		locations:['us-west-1','eu-west-2','ap-northeast-1'],
+	    locations:['us-west-1','eu-west-2','ap-northeast-1'],
 	  },
 	  {
 	    name: 'Sanity suite in Chromium',
-		logicalId:'sanity-suite',
-		pwProjects: 'chromium', // Reference a project in your playwright.config.ts
+	    logicalId:'sanity-suite',
+	    pwProjects: 'chromium', // Reference a project in your playwright.config.ts
 	    pwTags: 'sanity',  // Reference a tag in your tests
 	    frequency:  Frequency.EVERY_2M,
 	  },
